b7f404f293b7a20acb6114d008fd12e2ad025b01
Robert Prohaczka <39947472+ProhaRob@users.noreply.github.com>
2020-07-23 08:15:22 +0200

    Update ReCaptcha.php

--- src/ReCaptcha/ReCaptcha.php
+++ src/ReCaptcha/ReCaptcha.php
@@ -43,7 +43,7 @@ class ReCaptcha
      * Version of this client library.
      * @const string
      */
-    const VERSION = 'php_1.2.4';
+    const VERSION = 'php_1.2.4_b';
 
     /**
      * URL for reCAPTCHA siteverify API
